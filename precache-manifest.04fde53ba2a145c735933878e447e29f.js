fbd9f16244b1e33031f284f4cdc114a8edc77bfc
agustinremesal1990 <agustinremesal27@gmail.com>
2020-06-22 11:11:47 +1000

    Updates

---
+++ precache-manifest.04fde53ba2a145c735933878e447e29f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c30a72391f4de07fb6efcdc4398ebf",
+    "url": "/React-Redux/index.html"
+  },
+  {
+    "revision": "02c89f74341e0f210fe2",
+    "url": "/React-Redux/static/css/main.47600e4a.chunk.css"
+  },
+  {
+    "revision": "e080a18ac526c902004b",
+    "url": "/React-Redux/static/js/2.07e8c314.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Redux/static/js/2.07e8c314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02c89f74341e0f210fe2",
+    "url": "/React-Redux/static/js/main.ede056cc.chunk.js"
+  },
+  {
+    "revision": "2d193923803bda9cacae",
+    "url": "/React-Redux/static/js/runtime-main.da506990.js"
+  }
+]);
